8c2d15fc6da8b8c4ff32db80e7527e6a61a5a19d
Maanya <66780513+Maanya07@users.noreply.github.com>
2024-09-19 18:02:08 +0800

    Update problem2.main.kts

--- problem2.main.kts
+++ problem2.main.kts
@@ -1,19 +1,19 @@
-// Jagannathan, Maanya Problem 2 
+// Bingo problem
 
 fun bingoWord(word: String): String {
     // Code to get the uppercase version of the first letter 
     val firstLetter = word.first().uppercase()
     //code to get the length of the word
     val lengthOfWord = word.length
-    // Return the bingo word in the format "FirstLetter Length"
+    //returning the first letter & the length
     return "$firstLetter $lengthOfWord"
 }
 
 fun main() {
-    // Test the bingoWord function with different inputs
-    println("Bingo word for 'bingo' is: ${bingoWord("bingo")}")
-    println("Bingo word for 'Win' is: ${bingoWord("Win")}")
-    println("Bingo word for 'alex' is: ${bingoWord("alex")}")
+    // testing it 
+    println("Bingo word for 'Yes' is: ${bingoWord("yes")}")
+    println("Bingo word for 'Bob' is: ${bingoWord("bob")}")
+    println("Bingo word for 'life' is: ${bingoWord("life")}")
     println("Bingo word for 'kotlin' is: ${bingoWord("kotlin")}")
 }
 
